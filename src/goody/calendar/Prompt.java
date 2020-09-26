d00b5cf2010ff486b1870150ff1cc4b38e5681e6
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-09-26 22:04:31 +0900

    every month starts with sunday

---
+++ src/goody/calendar/Prompt.java
@@ -0,0 +1,40 @@
+package goody.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void rumprompt() {
+
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = 1;
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.println(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+
+			if (month > 12) {
+				continue;
+			}
+			
+			cal.printCalendar(2020, month);
+			
+		}
+
+		System.out.println("Bye~");
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		// 셸 실행
+		Prompt p = new Prompt();
+		p.rumprompt();
+	}
+}
